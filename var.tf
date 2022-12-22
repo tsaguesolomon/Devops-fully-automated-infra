beb087eb50eaad5de4ed9bdee0dde206ceea2ae1
Ekuba Tsague <solofashion33@gmail.com>
2022-12-22 07:37:45 -0500

    code improvement

--- var.tf
+++ var.tf
@@ -1,7 +1,7 @@
 variable "key_pair_name" {
     type = string
     description = "keypair to utilize"
-    default = "vamsi-chunduru"
+    default = "solofashion-keypair100"
 }
 
 # variable "vpc_id" {
